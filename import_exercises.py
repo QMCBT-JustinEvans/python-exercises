dd2d8a5fa9754015bd624aaa2315d194af7f1e8c
QMCBT-JustinEvans <Justin.Ellis.Evans@gmail.com>
2022-09-21 20:56:06 -0500

    ADD for assignment work; NOT Complete

---
+++ import_exercises.py
@@ -0,0 +1,9 @@
+from function_exercises import calculate_tip
+print('')
+print('')
+print('This is the answer to th import exercise.')
+print('')
+print('')
+calculate_tip(.10, 50.00)
+print('')
+print('')
